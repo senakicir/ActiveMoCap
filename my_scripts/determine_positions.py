reintroduced major bug again, fixed crop scales, start with constant rotation as predef motion

my_scripts/determine_positions.py
@@ -59,7 +59,7 @@ def determine_2d_positions(pose_client, current_state, my_rng, file_manager, lin
             heatmaps = create_heatmap(pose_2d.numpy().copy(), pose_client.SIZE_X,  pose_client.SIZE_Y)
 
     elif (pose_client.modes["mode_2d"] == "openpose"):  
-        pose_2d, heatmaps, _, _ =  openpose_module.run_only_model(cropped_image, [0.5, 0.75, 1, 1.25, 1.5, 2])
+        pose_2d, heatmaps, _, _ =  openpose_module.run_only_model(cropped_image, pose_client.cropping_tool.scales)
 
         pose_client.openpose_error = torch.mean(torch.norm(pose_2d_gt_cropped-pose_2d, dim=0))
         if not pose_client.USE_SINGLE_JOINT:
@@ -276,8 +276,8 @@ def determine_positions(linecount, pose_client, current_state, file_manager, my_
 
     if (plot_loc != 0 and not pose_client.quiet): 
         start_plot_time = time.time()
-        #check = pose_client.projection_client.take_single_projection(torch.from_numpy(pose_client.current_pose).float(), inv_transformation_matrix, camera_index)
-        #superimpose_on_image(pose_2d.cpu().numpy(), plot_loc, linecount, bone_connections, photo_loc, custom_name="projected_res_", scale = -1, projection=check.cpu().numpy())
+        check = pose_client.projection_client.take_single_projection(torch.from_numpy(pose_client.current_pose).float(), inv_transformation_matrix, camera_index)
+        superimpose_on_image(pose_2d.cpu().numpy(), plot_loc, linecount, bone_connections, photo_loc, custom_name="projected_res_", scale = -1, projection=check.cpu().numpy())
         #superimpose_on_image(pose_2d.numpy(), plot_loc, linecount, bone_connections, photo_loc, custom_name="projected_res_2_", scale = -1)
         #plot_2d_projection(check.numpy(), plot_loc, linecount, bone_connections, custom_name="proj_2d")
         if pose_client.modes["mode_2d"] == "openpose":
@@ -293,9 +293,9 @@ def determine_positions(linecount, pose_client, current_state, file_manager, my_
             #plot_future_poses(adjusted_optimized_poses, pose_client.FUTURE_WINDOW_SIZE, plot_loc, linecount, bone_connections, pose_client.animation)
             plot_all_optimization_results(adjusted_optimized_poses, pose_client.poses_3d_gt, pose_client.FUTURE_WINDOW_SIZE, plot_loc, linecount, bone_connections, pose_client.animation,  pose_client.errors, pose_client.average_errors)
             #plot_human(current_pose_3d_gt, pose_client.adj_current_pose, plot_loc, linecount-pose_client.MIDDLE_POSE_INDEX+1, bone_connections, pose_client.USE_SINGLE_JOINT, pose_client.animation, errors["middle_error"], custom_name="middle_pose_", label_names = ["GT", "Estimate"], additional_text = errors["ave_middle_error"])
-            #if pose_client.modes["mode_lift"] == "lift":
-            #hip_joint_gt = current_pose_3d_gt[:,hip_index]
-            #plot_human(current_pose_3d_gt-hip_joint_gt[:, np.newaxis], pose3d_lift_directions.numpy(), plot_loc, linecount, bone_connections, pose_client.USE_SINGLE_JOINT, pose_client.animation, -1, custom_name="lift_res_", label_names = ["LiftNet", "GT"])
+            if pose_client.modes["mode_lift"] == "lift":
+                hip_joint_gt = current_pose_3d_gt[:,hip_index]
+                plot_human(current_pose_3d_gt-hip_joint_gt[:, np.newaxis], pose3d_lift_directions.numpy(), plot_loc, linecount, bone_connections, pose_client.USE_SINGLE_JOINT, pose_client.animation, -1, custom_name="lift_res_", label_names = ["LiftNet", "GT"])
         end_plot_time = time.time()
         print("Time it took to plot", end_plot_time - start_plot_time)
     plot_end = {"est": pose_client.adj_current_pose, "GT": current_pose_3d_gt, "drone": current_state.C_drone_gt, "eval_time": func_eval_time}